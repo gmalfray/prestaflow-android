fbad61ce1cf0c4c1ad60757cfada84fdb59a8bab
Gregory MALFRAY <gregory.malfray.ext@corp.ovh.com>
2026-06-19 18:36:07 +0200

    feat(theme): défaut d'affichage en mode clair (cohérent maquettes Stitch)

--- app/src/main/java/com/rebuildit/prestaflow/domain/theme/ThemeSettings.kt
+++ app/src/main/java/com/rebuildit/prestaflow/domain/theme/ThemeSettings.kt
@@ -3,5 +3,5 @@ package com.rebuildit.prestaflow.domain.theme
 data class ThemeSettings(
     val skin: PrestaFlowSkin = PrestaFlowSkin.TERRACOTTA,
     val useDynamicColor: Boolean = false,
-    val darkThemeConfig: DarkThemeConfig = DarkThemeConfig.FOLLOW_SYSTEM,
+    val darkThemeConfig: DarkThemeConfig = DarkThemeConfig.LIGHT,
 )
