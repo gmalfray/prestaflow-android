6331d424e4d6ddfbaa4ff94df808fe05e6efb198
Gregory MALFRAY <gregory.malfray.ext@corp.ovh.com>
2026-06-23 22:41:14 +0200

    chore(release): version 0.1.16 (versionCode 17)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.rebuildit.prestaflow"
         minSdk = 26
         targetSdk = 36
-        versionCode = 16
-        versionName = "0.1.15"
+        versionCode = 17
+        versionName = "0.1.16"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
