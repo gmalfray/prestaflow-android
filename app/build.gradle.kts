ec8d1cba32d59135bc405488230ca75a9c4ffa93
Gregory MALFRAY <gregory.malfray.ext@corp.ovh.com>
2026-06-24 09:51:19 +0200

    chore(release): version 0.1.19 (versionCode 20)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.rebuildit.prestaflow"
         minSdk = 26
         targetSdk = 36
-        versionCode = 19
-        versionName = "0.1.18"
+        versionCode = 20
+        versionName = "0.1.19"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
