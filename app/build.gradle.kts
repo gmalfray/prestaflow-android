639cbc652d440a4361e4e1b3a809161cdb42264d
Gregory MALFRAY <gregory.malfray@kosmos.fr>
2026-06-28 17:16:54 +0200

    chore(release): app v0.6.0 (versionCode 29)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.rebuildit.prestaflow"
         minSdk = 26
         targetSdk = 36
-        versionCode = 28
-        versionName = "0.5.0"
+        versionCode = 29
+        versionName = "0.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
